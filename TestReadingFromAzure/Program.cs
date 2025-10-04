7d78d41801543614387eaf40f3ea5802c56a7108
SkyWakii <48031078+SkyWakii@users.noreply.github.com>
2025-10-04 09:04:05 +0300

    Initial commit

---
+++ TestReadingFromAzure/Program.cs
@@ -0,0 +1,31 @@
+﻿using Azure.Data.Tables;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        GetData("CpuUsage", (row) => $"CPU={row.GetDouble("CpuPercent")}");
+        GetData("MemoryUsage", (row) => $"Mem={row.GetDouble("MemUsedMb")}/{row.GetDouble("MemTotalMb")}");
+        GetData("PingTime", (row) => $"Ping={row.GetInt32("PingMs")}");
+    }
+    static void GetData(string tableName, Func<TableEntity, string> extractMessage)
+    {
+        var connectionString = "";
+        var tableClient = new TableClient(connectionString, tableName);
+
+        Console.WriteLine("Fetching last 10 rows...");
+        var machine = Environment.MachineName;
+
+        var query = tableClient.Query<TableEntity>(
+            filter: $"PartitionKey eq '{machine}'"
+        );
+
+        int count = 0;
+        foreach (TableEntity row in query)
+        {
+            Console.WriteLine($"{row.RowKey} | {extractMessage.Invoke(row)}");
+            if (++count >= 10) break;
+        }
+    }
+}
